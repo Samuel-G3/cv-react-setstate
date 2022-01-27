2d0b0e21f48a0946bb1c1839af4fb4d74007a398
Samuel-G3 <sgarciaine@gmail.com>
2022-01-27 17:21:42 +0100

    feat text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,11 +5,10 @@ const About = ({ hero, habilities }) => {
   return (
    
    <div className="card">
-      <div className="card-list__about"> 
       <p> {hero.aboutMe.info} </p>
-      </div>
+      
        <h3  className="card-title">Programación</h3>
-       <div className="card-list__program"> 
+       <div className="card-list"> 
        
         {hero.habilities.map((txt) => (
           <p key={txt}> {txt}</p>
